40994959556133bd9c75e2776ab91180809dff9c
Aleksandr <alesavigo@gmail.com>
2021-06-15 12:15:14 +0700

    Solvation for 23_rd task, general algorithm

--- 23_task/23_11358.py
+++ 23_task/23_11358.py
@@ -1,6 +1,21 @@
-n = 11
-posibilities = [0] * n
+def check_range(a, i):
+    global start, stop, condition
+    belong_gap = a in range(start, stop)
+    requir = belong_gap if i <= condition else (belong_gap and a >= condition)
+    a = int(a) if requir else 0
+    return a
 
-posibilities[3] = 1
-for i in range(3, n):
-    
+
+start = 3
+stop = 12 + 1
+condition = 10
+
+amount_path = [0] * stop
+amount_path[3] = 1
+
+for i in range(start + 1, stop):
+    amount_path[i] = amount_path[check_range(i - 1, i)]\
+                     + amount_path[check_range(i - 2, i)]\
+                     + amount_path[check_range(i / 2, i)]
+
+print(amount_path[stop - 1])
