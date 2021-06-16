2a941fb978652e40530740414e7c8454754d8fff
Aleksandr <56115945+AlSavIg@users.noreply.github.com+alesavigo@gmail.com>
2021-06-16 21:18:33 +0700

    What an idiot.

---
+++ 25_task/25_27422/main.py
@@ -0,0 +1,23 @@
+start = 174457
+n = 174505
+stop = n + 1
+dividers_dict = {}
+for i in range(start, stop):
+    divider = 2
+    dividers_count = 0
+    start_item = i
+    dividers_dict[start_item] = []
+    while divider ** 2 <= i:
+        if i % divider == 0:
+            i //= divider
+            dividers_count += 1
+            dividers_dict[start_item].append(divider)
+            if dividers_count > 2:
+                break
+        else:
+            divider += 1
+    if len(dividers_dict[start_item]) == 1:
+        dividers_dict[start_item].append(i)
+        key, value = start_item, dividers_dict[start_item]
+        # print("Number: {0}\nDividers: {1}    {2}".format(key, *value))
+        print(*value)
